4018faaaeeb29bf3bbe4543dcb6d49255e400c0b
mlaiki-taki <76485031+mlaiki-taki@users.noreply.github.com>
2021-04-09 04:23:45 +0200

    Add files via upload

---
+++ Jsxcheckpoint/src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import './style.css'
+function App() {
+  return (
+    <div className="container">
+
+     <div style={{border:"solid 1px black",maxWidth:"100vw"}}/>
+
+ <h1 className="title white">Mlaiki Med Taki Eddine </h1>
+
+ <br />
+
+ <img src="taki.jpg" alt="myphoto"/>
+
+ <br />
+
+ <img src="/softdev.png" alt="software development"/>
+
+ <br />
+
+<iframe title="react video" width="560" height="315" src="https://www.youtube.com/embed/N3AkSS5hXMA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+    </div>
+  );
+}
+
+export default App;
